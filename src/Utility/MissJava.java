0a400d73e636e5301908fe677a4b59d59b93365a
dilxat856988@gmail.com <dilxat856988@gmail.com>
2020-08-06 11:22:22 +1000

    add MissJava.java

---
+++ src/Utility/MissJava.java
@@ -0,0 +1,7 @@
+package Utility;
+
+public class MissJava {
+    // i miss java so much
+
+
+}
--- src/Utility/test.java
+++ src/Utility/test.java
@@ -1,3 +1,5 @@
+package Utility;
+
 public class test {
     public static void main(String[] args) {
 
